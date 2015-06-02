cd408c739d4a70bcc86613e6779d4354bc86f209
happyeti <adambukowiecki@wp.pl>
2015-06-02 11:31:47 +0200

    Bomb: getAllBombs

--- src/main/java/pl/edu/amu/wmi/sapper/map/objects/Bomb.java
+++ src/main/java/pl/edu/amu/wmi/sapper/map/objects/Bomb.java
@@ -1,4 +1,5 @@
 package pl.edu.amu.wmi.sapper.map.objects;
+
 import pl.edu.amu.wmi.sapper.map.objects.types.BombSize;
 import pl.edu.amu.wmi.sapper.map.objects.types.BombType;
 import pl.edu.amu.wmi.sapper.map.objects.types.Type;
