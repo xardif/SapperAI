e9534a1da4048de656d698d0625f64dda6f03ce1
mhybinski <mhybinski@users.noreply.github.com>
2015-06-07 17:31:56 +0200

    priorities

--- src/main/java/pl/edu/amu/wmi/sapper/clones/Skills.java
+++ src/main/java/pl/edu/amu/wmi/sapper/clones/Skills.java
@@ -5,9 +5,7 @@ import pl.edu.amu.wmi.sapper.map.objects.types.BombSize;
 import pl.edu.amu.wmi.sapper.map.objects.types.BombType;
 import pl.edu.amu.wmi.sapper.map.objects.types.Type;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 
 public class Skills {
@@ -23,9 +21,49 @@ public class Skills {
         Queue<BombType> result = tree.sortBombsByPriority(bombs);
         List<BombType> list = new ArrayList<>(result);
         System.out.println("Sorted bombs: " + list.toString());
+        List<Type> types = new ArrayList<>();
+        Map<String, Integer> numberOfBombs  = new HashMap<>();
 
-        for(int i=0; i<list.size();i++)
+
+        for(int i=0; i<list.size();i++){
+            types.add(list.get(i).getMaterial());
             System.out.println(list.get(i).getMaterial());
+        }
+
+        double priorityLevel =  1.0/(list.size());
+        int countC4=0;
+        int countChemicalBomb=0;
+        int countDirtyBomb=0;
+        int countDynamite=0;
+        int countFakeBomb=0;
+        int countNuke=0;
+        int countHomeMadeBomb=0;
+        double NukePriority=0.0;
+        double C4Priority=0.0;
+        double DirtyBobmPriority=0.0;
+        double FakeBombPriority=0.0;
+        double DynamitePriority=0.0;
+        double HomeMadeBombPriority=0.0;
+
+
+        for(int x=0;x<types.size();x++){
+            if(types.get(x)==Type.Nuke)
+                countNuke++;
+            else if(types.get(x)==Type.C4)
+                countC4++;
+            else if(types.get(x)==Type.ChemicalBomb)
+                countChemicalBomb++;
+            else if(types.get(x)==Type.DirtyBomb)
+                countDirtyBomb++;
+            else if(types.get(x)==Type.Dynamite)
+                countDynamite++;
+            else if(types.get(x)==Type.FakeBomb)
+                countFakeBomb++;
+            else if(types.get(x)==Type.HomeMadeBomb)
+                countHomeMadeBomb++;
+        }
+
+
 
         String skills="111110100000111110100";
         return skills;
