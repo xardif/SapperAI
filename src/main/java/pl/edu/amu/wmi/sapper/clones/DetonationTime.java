0ca16ad9a2d5e0df378e271123b6180ed4c62b32
s383958 <mh78514@st.amu.edu.pl>
2015-06-10 17:11:59 +0200

    fix

--- src/main/java/pl/edu/amu/wmi/sapper/clones/DetonationTime.java
+++ src/main/java/pl/edu/amu/wmi/sapper/clones/DetonationTime.java
@@ -19,53 +19,53 @@ public class DetonationTime {
         part[5]=skills.substring(10,12);
         part[6]=skills.substring(12,14);
 
-        if(part[0]=="01")
+        if(part[0].equals("01"))
             bomby.put(Type.Nuke, 10);
-        else if(part[0]=="10")
+        else if(part[0].equals("10"))
             bomby.put(Type.Nuke, 7);
-        else if(part[0]=="11")
+        else if(part[0].equals("11"))
             bomby.put(Type.Nuke, 4);
 
-        if(part[1]=="01")
+        if(part[1].equals("01"))
             bomby.put(Type.C4, 10);
-        else if(part[1]=="10")
+        else if(part[1].equals("01"))
             bomby.put(Type.C4, 7);
-        else if(part[1]=="11")
+        else if(part[1].equals("11"))
             bomby.put(Type.C4, 4);
 
-        if(part[2]=="01")
+        if(part[2].equals("01"))
             bomby.put(Type.DirtyBomb, 10);
-        else if(part[2]=="10")
+        else if(part[2].equals("10"))
             bomby.put(Type.DirtyBomb, 7);
-        else if(part[2]=="11")
+        else if(part[2].equals("11"))
             bomby.put(Type.DirtyBomb, 4);
 
-        if(part[3]=="01")
+        if(part[3].equals("01"))
             bomby.put(Type.ChemicalBomb, 10);
-        else if(part[3]=="10")
+        else if(part[3].equals("10"))
             bomby.put(Type.ChemicalBomb, 7);
-        else if(part[3]=="11")
+        else if(part[3].equals("11"))
             bomby.put(Type.ChemicalBomb, 4);
 
-        if(part[4]=="01")
+        if(part[4].equals("01"))
             bomby.put(Type.Dynamite, 10);
-        else if(part[4]=="10")
+        else if(part[4].equals("10"))
             bomby.put(Type.Dynamite, 7);
-        else if(part[4]=="11")
+        else if(part[4].equals("11"))
             bomby.put(Type.Dynamite, 4);
 
-        if(part[5]=="01")
+        if(part[5].equals("01"))
             bomby.put(Type.HomeMadeBomb, 10);
-        else if(part[5]=="10")
+        else if(part[5].equals("10"))
             bomby.put(Type.HomeMadeBomb, 7);
-        else if(part[5]=="11")
+        else if(part[5].equals("11"))
             bomby.put(Type.HomeMadeBomb, 4);
 
-        if(part[6]=="01")
+        if(part[6].equals("01"))
             bomby.put(Type.FakeBomb, 10);
-        else if(part[6]=="10")
+        else if(part[6].equals("10"))
             bomby.put(Type.FakeBomb, 7);
-        else if(part[6]=="11")
+        else if(part[6].equals("11"))
             bomby.put(Type.FakeBomb, 4);
 
         return (bomby);
