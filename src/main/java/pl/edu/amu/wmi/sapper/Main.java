b45e3bd587d1ba85a89cb58367b47882aca15d73
happyeti <adambukowiecki@wp.pl>
2015-06-10 16:05:33 +0200

    Merge

--- src/main/java/pl/edu/amu/wmi/sapper/Main.java
+++ src/main/java/pl/edu/amu/wmi/sapper/Main.java
@@ -1,6 +1,15 @@
 package pl.edu.amu.wmi.sapper;
 
+import java.awt.Image;
+import java.io.IOException;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import pl.edu.amu.wmi.sapper.ai.SapperLogic;
+import pl.edu.amu.wmi.sapper.ai.neural.BombRecognize;
 import pl.edu.amu.wmi.sapper.map.Map;
 import pl.edu.amu.wmi.sapper.map.objects.Blockade;
 import pl.edu.amu.wmi.sapper.map.objects.Bomb;
@@ -9,78 +18,46 @@ import pl.edu.amu.wmi.sapper.map.objects.Sapper;
 import pl.edu.amu.wmi.sapper.map.objects.types.BombSize;
 import pl.edu.amu.wmi.sapper.map.objects.types.BombType;
 import pl.edu.amu.wmi.sapper.map.objects.types.Type;
+import pl.edu.amu.wmi.sapper.ui.ImageUtil;
+import pl.edu.amu.wmi.sapper.util.JsonParser;
 
 public class Main {
 
-	public static void init(Map map) {
-		//bomba
-		BombType b = new BombType(10, Type.C4, BombSize.MEDIUM, 180, true);
-		map.setField(15, 15, new Bomb());
-		
-		//blokady
-		map.setField(2, 3, new Blockade());
-		map.setField(2, 4, new Blockade());
-		map.setField(2, 5, new Blockade());
-		map.setField(3, 5, new Blockade());
-		map.setField(4, 5, new Blockade());
-		map.setField(5, 5, new Blockade());
-		map.setField(6, 5, new Blockade());
-		map.setField(7, 5, new Blockade());
-		map.setField(8, 5, new Blockade());
-		map.setField(8, 4, new Blockade());
-		map.setField(8, 3, new Blockade());
-		map.setField(8, 2, new Blockade());
-		map.setField(8, 1, new Blockade());
-		
-		map.setField(0, 8, new Blockade());
-		map.setField(0, 9, new Blockade());
-		map.setField(2, 8, new Blockade());
-		map.setField(2, 9, new Blockade());
-		map.setField(2, 10, new Blockade());
-		map.setField(3, 10, new Blockade());
-		map.setField(4, 10, new Blockade());
-		map.setField(5, 10, new Blockade());
-		map.setField(6, 10, new Blockade());
-		map.setField(7, 10, new Blockade());
-		map.setField(8, 10, new Blockade());
-		map.setField(8, 9, new Blockade());
-		map.setField(8, 8, new Blockade());
-		map.setField(8, 7, new Blockade());
-		
-		for(int i=1; i<14; i++)
-			map.setField(12, i, new Blockade());
-		
-		map.setField(14, 14, new Blockade());
-		map.setField(14, 15, new Blockade());
-		
-		//cywile
-		map.setField(11, 15, new Civilians(20));
-		map.setField(13, 12, new Civilians(87));
-		map.setField(13, 14, new Civilians(100));
-		map.setField(15, 10, new Civilians(11));
-
-
-		//saper
-		SapperLogic s = new SapperLogic(map.getField(0, 0));
+	
+	/**
+	 * 		SapperLogic s = new SapperLogic(map.getField(0, 0));
 		map.setField(0, 0, new Sapper());
 		s.findPath(s.getField(), map.getField(15, 15), map);
-	}
-	
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		Map map = new Map(18,18);
-		init(map);
 		
 		/*
-		Field f = new Field();
-		f.getObjects().add(new Bomb());
-		
-		for(FieldObject obj: f.getObjects()) {
-			if( obj instanceof Bomb ) {
-				Bomb bomb = (Bomb) obj;
-				System.out.println("radius = " + bomb.getBombType().getRadius());
-			}		
-		}
+		JFileChooser chooser = new JFileChooser();
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	        "JSON file", "json");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(null);
+	    String path = chooser.getSelectedFile().getAbsolutePath();
 		*/
+		Map map = null;
+	    try {
+			map = JsonParser.parse("/map/main.json");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    
+	    List<Bomb> bombs = map.getAllBombs();
+	    BombRecognize recognizer = new BombRecognize();
+	    
+		for(Bomb bomb: bombs) {
+			Image img = new ImageIcon(BombRecognize.class.getResource(bomb.getPathToTypeImage())).getImage();
+			BombType bombType = new BombType(bomb);
+			bombType.setType(Type.valueOf(recognizer.recognize(img)));
+		}
+		
+		
+		
 	}
 
 }
