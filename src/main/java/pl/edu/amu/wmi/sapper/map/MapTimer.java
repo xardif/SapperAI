ddde29fa50d9e68d4dac27cba89320697f07918a
happyeti <adambukowiecki@wp.pl>
2015-06-11 08:51:44 +0200

    MapTimer

---
+++ src/main/java/pl/edu/amu/wmi/sapper/map/MapTimer.java
@@ -0,0 +1,35 @@
+package pl.edu.amu.wmi.sapper.map;
+
+public class MapTimer implements Runnable {
+
+	private int time = 0;
+	public int getTime() {
+		return time;
+	}
+
+	private Map map;
+	
+	public MapTimer(Map m) {
+		map = m;
+	}
+	
+	@Override
+	public void run() {
+		
+		while(true) {
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			time++;
+			
+			map.checkBombs();
+			
+		}
+
+	}
+
+}
